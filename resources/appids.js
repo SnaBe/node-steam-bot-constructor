c18d31b7d0df8a74b723bdd532207edc91efba7c
Simon Sørensen <fynboen1999@gmail.com>
2019-05-17 21:47:12 +0200

    Add more resources
    For logger added levels.js, for Steam added appids.js for games and contextids.js for getting inventories.

---
+++ resources/appids.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TF2:   440,
+    DOTA2: 570,
+    CSGO:  730,
+    Steam: 753
+}
---
+++ resources/contextids.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TF2:   2,
+    DOTA2: 2,
+    CSGO:  2,
+    Steam: 6
+}

29758b8f36264eb8f23ee9c467e13c5d903a640f
Simon Sørensen <fynboen1999@gmail.com>
2019-05-17 22:19:10 +0200

    Added config requirements

--- examples/app.js
+++ examples/app.js
@@ -1,14 +1,15 @@
+const config = require('./settings/config.json');
 const SteamBot = require('../index');
 const SteamTotp = require('steam-totp');
 
 const tradeBot = new SteamBot({
-  accountName: "tradingbotforsnabe",
-  password: "tradingbotforsteam",
-  twoFactorCode: SteamTotp.generateAuthCode("Zl9HB6l9eA76Lpv9Wizwq8enSls=")
+  accountName: config.tradeBot.username,
+  password: config.tradeBot.password,
+  twoFactorCode: SteamTotp.generateAuthCode(config.tradeBot.sharedSecret)
 });
 
 const idleBot = new SteamBot({
-  accountName: "tradingbotforsnabe",
-  password: "tradingbotforsteam",
-  twoFactorCode: SteamTotp.generateAuthCode("Zl9HB6l9eA76Lpv9Wizwq8enSls=")
+  accountName: config.idleBot.username,
+  password: config.idleBot.password,
+  twoFactorCode: SteamTotp.generateAuthCode(config.tradeBot.sharedSecret)
 });

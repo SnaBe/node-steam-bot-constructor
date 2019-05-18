4f1b613fa63c5de1bfc25f08203afd65af11905d
Simon Sørensen <fynboen1999@gmail.com>
2019-05-18 16:37:13 +0200

    Update to the primary example
    Added example for client.gamesPlayed, manager.on('newOffer') & a custom function.

--- examples/app.js
+++ examples/app.js
@@ -1,15 +1,44 @@
+//Import libs & resources
 const config = require('./settings/config.json');
+const logger = require('../lib/logger');
+const AppID = require('../resources/appids');
+//Replace this with `const SteamBot = require('steam-bot-constructor');` if used outside of the module directory
 const SteamBot = require('../index');
+//Other required modules
 const SteamTotp = require('steam-totp');
 
+//Setup a new SteamBot instance named tradeBot
 const tradeBot = new SteamBot({
   accountName: config.tradeBot.username,
   password: config.tradeBot.password,
   twoFactorCode: SteamTotp.generateAuthCode(config.tradeBot.sharedSecret)
 });
 
+//Setup a new SteamBot instance named idleBot
 const idleBot = new SteamBot({
   accountName: config.idleBot.username,
   password: config.idleBot.password,
   twoFactorCode: SteamTotp.generateAuthCode(config.tradeBot.sharedSecret)
 });
+
+//The tradeBot instance will play CSGO once it logs onto Steam.
+tradeBot.client.on('loggedOn', function(details) {
+  tradeBot.client.gamesPlayed(AppID.TF2);
+});
+
+//The idleBot instance will play TF2 once it logs onto Steam.
+idleBot.client.on('loggedOn', function(details) {
+  idleBot.client.gamesPlayed(AppID.DOTA2);
+});
+
+//When the tradeBot instance receives a new trade offer, act on it.
+tradeBot.manager.on('newOffer', function(offer) {
+  logger.warn(`New offer #${offer.id} from ${offer.partner.getSteamID64()}. Offer note: ${offer.message}`);
+  //Call a custom function 
+  processOffer(offer);
+});
+
+//Your custom function to process the parsed offer.
+function processOffer(offer) {
+  logger.info(`Processing offer #${offer.id}`);
+}
